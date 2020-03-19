96faa27495f0d4edc6cff379e5f57a2a8c21e1c7
janecyyu <janecyyu@gmail.com>
2020-03-18 19:22:34 -0700

    add button

--- src/component/middle-content/m-card.js
+++ src/component/middle-content/m-card.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./m-content.css";
 
 const StoryCard = props => {
   return (
-    <div className="content-container">
-      <h2>Author: {props.copyright}</h2>
-      <h6>Date: {props.date}</h6>
-      <p>{props.explanation}</p>
+    <div className="middle">
+        <div className="content-container">
+            <h2>Author: {props.copyright}</h2>
+            <h6>Date: {props.date}</h6>
+            <p>{props.explanation}</p>
+        </div>
+        <div className="img-container">
+            <img className="m-img" src={props.hdurl} alt="photo from nasa"/>
+        </div>
     </div>
   );
 };
--- src/component/middle-content/m-content.js
+++ src/component/middle-content/m-content.js
@@ -3,27 +3,27 @@ import "./m-content.css";
 import axios from "axios";
 import StoryCard from "./m-card";
 
+var currentDate = new Date();
 var today = new Date();
 
-var prev_date = new Date();
-prev_date.setDate(prev_date.getDate() - 1);
+// var prev_date = new Date();
+// prev_date.setDate(prev_date.getDate() - 1);
 
-var next_date = new Date();
-next_date.setDate(prev_date.getDate() + 1);
+// var next_date = new Date();
+// next_date.setDate(prev_date.getDate() + 1);
 
 function dateMaker(date) {
   var dd = String(date.getDate()).padStart(2, "0");
   var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
   var yyyy = date.getFullYear();
   date = yyyy + "-" + mm + "-" + dd;
-  today = date;
   return date;
 }
 
 const MiddleContent = () => {
   //console.log(dateMaker(next_date));
   //console.log("today" + today);
-  const [date, setDate] = useState(dateMaker(today));
+  const [date, setDate] = useState(dateMaker(currentDate));
   const [info, setInfo] = useState([]);
   useEffect(() => {
     axios
@@ -35,18 +35,40 @@ const MiddleContent = () => {
         setInfo(re.data);
       })
       .catch(err => console.log("error", err));
-    return () => console.log("The Effect Hook has been cleaned up.");
   }, [date]);
   return (
-    <div className="middle">
-      <button onClick={() => setDate(dateMaker(prev_date))}>previous</button>
-      <button onClick={() => setDate(dateMaker(today))}>today</button>
-      <button onClick={() => setDate(dateMaker(next_date))}>next</button>
+    <div className="m">
+      <button
+        onClick={() => {
+          currentDate.setDate(currentDate.getDate() - 1);
+          setDate(dateMaker(currentDate));
+        }}
+      >
+        previous
+      </button>
+      <button
+        onClick={() => {
+            currentDate.setDate(today.getDate());
+            setDate(dateMaker(today));
+        }}
+      >
+        today
+      </button>
+      <button
+        onClick={() => {
+          currentDate.setDate(currentDate.getDate() + 1);
+          setDate(dateMaker(currentDate));
+        }}
+      >
+        next
+      </button>
+
       <div className="entry">
         <StoryCard
           date={info.date}
           explanation={info.explanation}
           copyright={info.copyright}
+          hdurl={info.hdurl}
         />
       </div>
     </div>
