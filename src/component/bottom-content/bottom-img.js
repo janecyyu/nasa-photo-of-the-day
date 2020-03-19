f55d0c6d1d7b48cb1befbf08ab6f6a57f9212ac2
janecyyu <janecyyu@gmail.com>
2020-03-18 19:31:18 -0700

    add one more picture

--- src/component/bottom-content/bottom-img.js
+++ src/component/bottom-content/bottom-img.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./header.css" 
+import "./bottom-img.css"
 
-export default function Header() {
+export default function Bottom() {
   const [bgc, setBgc] = useState([]);
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?date=2020-01-16&api_key=2h7Qg2rbJK3WTXXDKIDySG0VQeaCHWLdtW9eLgb2")
+      .get("https://api.nasa.gov/planetary/apod?date=2019-01-16&api_key=2h7Qg2rbJK3WTXXDKIDySG0VQeaCHWLdtW9eLgb2")
       .then(re => {
         console.log(re.data.hdurl);
         setBgc(re.data.hdurl);
